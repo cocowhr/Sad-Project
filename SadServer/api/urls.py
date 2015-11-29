ecebe85b759f74be2973c3e90533446f450c1bc2
cocowhr <whr4545@yeah.net>
2015-11-30 01:24:12 +0900

    Android

--- SadServer/api/urls.py
+++ SadServer/api/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^searchhospname/$', 'api.views.searchhospname'),
     url(r'^searchdepartment/$', 'api.views.searchdepartment'),
     url(r'^orders/(?P<hospitalid>\d+)/$','api.views.searchdepartment'''),
+    #Android
+    url(r'^loginandroid/$', 'api.views.loginandroid'),
+    url(r'^loginandroid/$', 'django.contrib.auth.views.login'),
+    url(r'^registerandroid/$', 'api.views.register_page'),
 ]
